d086673e43f056bc663c357cca7446d5e90155dd
Tinesh <tineshbabukatta@gmail.com>
2021-10-09 23:30:02 +0530

    modify folder

--- vars/mavenGoals.groovy
+++ vars/mavenGoals.groovy
@@ -1,6 +1,6 @@
-def call(Map config) {
+def call(Map config = [:]) {
 
-    sh "cd ${workspace}/config.name/"
+    sh "cd ${workspace}/${config.name}/"
     sh "ls -altr"
     sh "Building the Code Now"
     sh "mvn clean package"
