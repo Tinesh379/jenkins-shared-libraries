516541db4a25fcc79a9813833f60b0893ae20bce
Tinesh <tineshbabukatta@gmail.com>
2021-10-10 11:09:06 +0530

    copy/paste nexus snippet from google

--- vars/nexusArtifacts.groovy
+++ vars/nexusArtifacts.groovy
@@ -1,19 +1,42 @@
 def call(Map config = [:]) {
 
-    pom = readMavenPom file: 'pom.xml'
-    filesByGlob = findFiles excludes: '', glob: 'target/*.${pom.packaging}'
-    artifactPath = filesByGlob[0].path
+    pom = readMavenPom file: "pom.xml";
+                    // Find built artifact under target folder
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    // Print some info from the artifact found
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    // Extract the path from the File found
+                    artifactPath = filesByGlob[0].path;
+                    // Assign to a boolean response verifying If the artifact name exists
+                    artifactExists = fileExists artifactPath;
 
-    nexusArtifactUploader artifacts: 
-    [[artifactId: 'pom.artifactId', 
-    classifier: '', 
-    file: artifactPath,
-    type: 'pom.packaging']], 
-    credentialsId: 'nexus', 
-    groupId: 'pom.groupId', 
-    nexusUrl: '192.168.1.101:8081', 
-    nexusVersion: 'nexus3', 
-    protocol: 'http', 
-    repository: config.repository, 
-    version: 'pom.version'
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+
+                        nexusArtifactUploader(
+                            nexusVersion: "nexus3",
+                            protocol: "http",
+                            nexusUrl: "localhost:8080",
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: config.repository,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                // Artifact generated such as .jar, .ear and .war files.
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+
+                                // Lets upload the pom.xml file for additional information for Transitive dependencies
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                    }
 }
