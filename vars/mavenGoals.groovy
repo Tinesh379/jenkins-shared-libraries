68ac739fa1475d1d13444babff54f9158aaeb254
Tinesh379 <85128084+Tinesh379@users.noreply.github.com>
2021-10-11 11:20:47 +0530

    Update mavenGoals.groovy

--- vars/mavenGoals.groovy
+++ vars/mavenGoals.groovy
@@ -1,7 +1,17 @@
 def call(Map config = [:]) {
-
+    
+    if(isUnix()){
+        
     sh "cd ${workspace}/"
     sh "ls -altr"
     sh "echo 'Building the Code Now'"
     sh "mvn clean package"
+    }
+    else{
+        
+    bat "cd ${workspace}/"
+    bat "dir"
+    bat "echo 'Building the Code Now'"
+    bat "mvn clean package"
+             
 }
